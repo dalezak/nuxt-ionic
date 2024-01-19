bf1eabe85e851665283d4c5ed6e1c7d4a949d429
Dale Zak <dalezak@gmail.com>
2024-01-19 07:28:58 -0600

    Updated app tabs

--- app.config.ts
+++ app.config.ts
@@ -4,26 +4,26 @@ export default defineAppConfig({
   theme: "default",
   dark: false,
   tabs: [
-    {
-      name: "home",
-      label: "Home",
-      path: "/home",
-      icon: "home",
-      auth: true
-    },
-    {
-      name: "about",
-      label: "About",
-      path: "/about",
-      icon: "informationCircle",
-      auth: false
-    },
-    {
-      name: "login",
-      label: "Login",
-      path: "/login",
-      icon: "key",
-      auth: false
-    }
+    // {
+    //   name: "home",
+    //   label: "Home",
+    //   path: "/home",
+    //   icon: "home",
+    //   auth: true
+    // },
+    // {
+    //   name: "about",
+    //   label: "About",
+    //   path: "/about",
+    //   icon: "informationCircle",
+    //   auth: false
+    // },
+    // {
+    //   name: "login",
+    //   label: "Login",
+    //   path: "/login",
+    //   icon: "key",
+    //   auth: false
+    // }
   ]
 })
