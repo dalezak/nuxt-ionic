627d498125832a886ab9193e1fa9d5fa24c724ea
Dale Zak <dalezak@gmail.com>
2024-01-25 07:23:45 -0600

    Added debug to showPageIndex

--- utils/show-page-index.js
+++ utils/show-page-index.js
@@ -5,9 +5,11 @@ export default function () {
   const appTabs = tabs.filter(tab => tab.public == noUser);
   if (appTabs.length > 0) {
     const tab = appTabs[0];
+    consoleLog("showPageIndex", tab.name);
     showPage(tab.path, false);
   }
   else {
+    consoleLog("showPageIndex", "index");
     showPage("/", false);
   }
 }
