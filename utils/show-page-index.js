3ca8bc2a243036739b296705fdee4b28bb8f8851
Dale Zak <dalezak@gmail.com>
2024-02-06 07:44:36 -0600

    Updated tabs

--- utils/show-page-index.js
+++ utils/show-page-index.js
@@ -3,8 +3,8 @@ export default function () {
   const { tabs } = useAppConfig();
   const appTabs = tabs.filter(tab => tab.public == !appUser.value);
   consoleLog("showPageIndex", "tabs", appTabs.value);
-  if (appTabs.value.length > 0) {
-    const tab = appTabs.value[0];
+  if (appTab.length > 0) {
+    const tab = appTabs[0];
     consoleLog("showPageIndex", "path", tab.path);
     showPage(tab.path, false);
   }
