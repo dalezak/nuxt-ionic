aa2bf44b6336f98fdfbcac4a413799e055a84eb8
Dale Zak <dalezak@gmail.com>
2024-03-19 07:39:51 -0600

    Added share-social

---
+++ utils/share-custom.js
@@ -0,0 +1,42 @@
+export default async function (title, text, url) {
+  let actionSheet = await actionSheetController.create({
+    header: 'Share Via',
+    buttons: [
+      {
+        text: 'Twitter',
+        handler: () => {
+            shareTwitter(title, text, url);
+        }
+      },
+      {
+        text: 'Facebook',
+        handler: () => {
+            shareFacebook(title, text, url);
+        }
+      },
+      {
+        text: 'LinkedIn',
+        handler: () => {
+            shareLinkedIn(title, text, url);
+        }
+      },
+      {
+        text: 'Pinterest',
+        handler: () => {
+            sharePinterest(title, text, url);
+        }
+      },
+      {
+        text: 'Email',
+        handler: () => {
+            shareEmail(title, text, url);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      },
+    ],
+  });
+  await actionSheet.present();
+}
---
+++ utils/share-social.js
@@ -0,0 +1,18 @@
+import { Share } from '@capacitor/share';
+export default async function (title, text, url) {
+  let canShare = await Share.canShare();
+  if (canShare && canShare.value) {
+      let options = {};
+      if (title && title.length > 0) {
+          options["title"] = title;
+      }
+      if (text && text.length > 0) {
+          options["text"] = text;
+      }
+      if (url && url.length > 0) {
+          options["url"] = url;
+      }
+      return await Share.share(options);
+  }
+  return await shareCustom(title, text, url);
+}
