519d1ff95f8d5ce5980e33d983cba506b18939b4
Dale Zak <dalezak@gmail.com>
2024-03-27 07:48:53 -0600

    Updated sharing

--- utils/share-custom.js
+++ utils/share-custom.js
@@ -1,29 +1,39 @@
 export default async function ({title, description, url}) {  
   return await showActionSheet({
-    "Email": shareEmail({
-      title: title, 
-      description: description, 
-      url: url
-    }),
-    "Twitter": shareTwitter({
-      title: title, 
-      description: description, 
-      url: url
-    }),
-    "Facebook": shareFacebook({
-      title: title, 
-      description: description, 
-      url: url
-    }),
-    "LinkedIn": shareLinkedIn({
-      title: title, 
-      description: description, 
-      url: url
-    }),
-    "Pinterest": sharePinterest({
-      title: title, 
-      description: description, 
-      url: url
-    })
+    "Email": () => { 
+      shareEmail({
+        title: title, 
+        description: description, 
+        url: url
+      })
+    },
+    "Twitter": () => { 
+      shareTwitter({
+        title: title, 
+        description: description, 
+        url: url
+      })
+    },
+    "Facebook": () => {
+      shareFacebook({
+        title: title, 
+        description: description, 
+        url: url
+      })
+    },
+    "LinkedIn": () => { 
+      shareLinkedIn({
+        title: title, 
+        description: description, 
+        url: url
+      })
+    },
+    "Pinterest": () => { 
+      sharePinterest({
+        title: title, 
+        description: description, 
+        url: url
+      })
+    }
   });
 }
