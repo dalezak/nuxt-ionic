04873284570267740edd6edf4c26a9b7b8373268
Dale Zak <dalezak@gmail.com>
2024-04-03 17:09:20 -0600

    Trying useIonRouter

--- utils/show-page.js
+++ utils/show-page.js
@@ -1,9 +1,11 @@
 export default function (path, push = true) {
   if (process.client) {
-    const router = useRouter();
+    // const router = useRouter();
+    const router = useIonRouter();
     if (push) {
       consoleLog("showPage", path);
-      router.push({ path: path });
+      // router.push({ path: path });
+      router.push(path);
     }
     else {
       consoleLog("showPage", path);
